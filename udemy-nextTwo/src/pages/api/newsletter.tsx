bc1a41c5d014855f8db96052ad313bb4f3917ef0
KooTaehyeon <vcro12123@naver.com>
2023-06-28 13:43:27 +0900

    next 몽고디비 연결

--- udemy-nextTwo/src/pages/api/newsletter.tsx
+++ udemy-nextTwo/src/pages/api/newsletter.tsx
@@ -1,6 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+const uri =
+  'mongodb+srv://vcro12123:05321zxc@cluster0.6po88go.mongodb.net/newsletter';
 
-function handler(req: NextApiRequest, res: NextApiResponse) {
+async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
 
@@ -8,6 +11,12 @@ function handler(req: NextApiRequest, res: NextApiResponse) {
       res.status(422).json({ message: 'invalid email address.' });
       return;
     }
+    const client = await MongoClient.connect(uri);
+    const db = client.db();
+    await db.collection('emails').insertOne({
+      email: userEmail,
+    });
+    client.close;
     console.log(userEmail);
     res.status(201).json({ message: '성공' });
   }
